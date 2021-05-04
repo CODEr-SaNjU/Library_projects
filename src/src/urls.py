4e81e495c17da3476777896e0c6173384c9d1e2f
coder-sanju <sanju2help@gmail.com>
2021-05-04 10:24:31 +0530

    project setup installtion

--- src/src/urls.py
+++ src/src/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
+from backend import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("backend.urls")),
     path('__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
