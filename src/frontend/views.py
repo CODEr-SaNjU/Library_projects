feccb5bdbf4e934d2f4f1efaf0c451d453c9e9a2
coder-sanju <sanju2help@gmail.com>
2021-05-04 10:35:33 +0530

    finished project setup

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def Dashobard(request):
+    return render(request, 'Books_html/main.htm')
--- src/src/urls.py
+++ src/src/urls.py
@@ -1,11 +1,12 @@
-import debug_toolbar
 from django.conf.urls.static import static
+import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from backend import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("backend.urls")),
+    path('api/', include("backend.urls")),
+    path('', include('frontend.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
